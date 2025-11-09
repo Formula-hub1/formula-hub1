c92561f5c60b5ab8124177150be867f7cc18a211
Alejandro Parody <alejandroparodyq@gmail.com>
2025-11-09 15:19:41 +0100

    feat: implemented fakenodo funtionalities in routes

--- app/modules/fakenodo/routes.py
+++ app/modules/fakenodo/routes.py
@@ -1,7 +1,53 @@
-from flask import render_template
+from flask import render_template, jsonify, make_response
 from app.modules.fakenodo import fakenodo_bp
 
+base_url = "/fakenodo/api"
 
-@fakenodo_bp.route('/fakenodo', methods=['GET'])
-def index():
-    return render_template('fakenodo/index.html')
+@fakenodo_bp.route(base_url, methods=['GET'])
+def test_fakenodo():
+    response = {
+        "status": "success",
+        "message": "FakeNododo API is working!",
+    }
+    return jsonify(response)
+
+@fakenodo_bp.route(base_url, methods=['POST'])
+def create_fakenodo():
+    response = {
+        "status": "success",
+        "message": "FakeNododo created successfully!",
+    }
+    return make_response(jsonify(response), 201)
+
+@fakenodo_bp.route(base_url + '/<depositionId>/files', methods=['POST'])
+def deposition_files_fakenodo(depositionId):
+    response = {
+        "status": "success",
+        "message": f"Created deposition {depositionId} successfully!",
+    }
+    return make_response(jsonify(response), 201)
+
+@fakenodo_bp.route(base_url + '/<depositionId>', methods=['DELETE'])
+def delete_deposition_fakenodo(depositionId):
+    response = {
+        "status": "success",
+        "message": f"Deleted deposition {depositionId} successfully!",
+    }
+    return make_response(jsonify(response), 200)
+
+@fakenodo_bp.route(base_url + '/<depositionId>/actions/publish', methods=['POST'])
+def publish_deposition_fakenodo(depositionId):
+    response = {
+        "status": "success",
+        "message": f"Published deposition {depositionId} successfully!",
+    }
+    return make_response(jsonify(response), 202)
+
+@fakenodo_bp.route(base_url + '/<depositionId>', methods=['GET'])
+def get_deposition_fakenodo(depositionId):
+    response = {
+        "status": "success",
+        "message": f"Fetched deposition {depositionId} successfully!",
+        "doi": "10.1234/fakenodo.123456",
+    }
+    return make_response(jsonify(response), 200)
--- app/modules/zenodo/services.py
+++ app/modules/zenodo/services.py
@@ -22,14 +22,14 @@ class ZenodoService(BaseService):
     def get_zenodo_url(self):
 
         FLASK_ENV = os.getenv("FLASK_ENV", "development")
-        ZENODO_API_URL = ""
+        ZENODO_API_URL = "hhtp://localhost/fakenodo/api"
 
-        if FLASK_ENV == "development":
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
-        elif FLASK_ENV == "production":
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://zenodo.org/api/deposit/depositions")
-        else:
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
+        #if FLASK_ENV == "development":
+        #    ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
+        #elif FLASK_ENV == "production":
+        #    ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://zenodo.org/api/deposit/depositions")
+        #else:
+        #    ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
 
         return ZENODO_API_URL
 
