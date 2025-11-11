c5a86e63912e5c32c510ae47cd390637551ea7ef
Alberto Garcia Sanz <albertogarsa04@gmail.com>
2025-11-11 10:41:01 +0100

    feature: Modified algorythm so it updates the suggested datasets every hour

--- app/modules/dataset/models.py
+++ app/modules/dataset/models.py
@@ -93,7 +93,8 @@ class DataSet(db.Model):
 
     ds_meta_data_id = db.Column(db.Integer, db.ForeignKey("ds_meta_data.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
+    recalculated_at = db.Column(db.DateTime, nullable=True)
+    
     ds_meta_data = db.relationship("DSMetaData", backref=db.backref("data_set", uselist=False))
     feature_models = db.relationship("FeatureModel", backref="data_set", lazy=True, cascade="all, delete")
 
