3dfede602ade885f447ff8742508a94135fd0db9
Alejandro Parody <alejandroparodyq@gmail.com>
2025-12-03 19:05:56 +0100

    fix: conform module files to linter rules

--- app/modules/uploader/tests/test_selenium.py
+++ app/modules/uploader/tests/test_selenium.py
@@ -1,28 +1,21 @@
-import unittest
 import os
+import time
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-import time
 
+TEST_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files", "test1.zip")
 
-TEST_FILE_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'test_files', 'test1.zip'
-)
-
-REPO_ZIP_URL = (
-    "https://github.com/Universal-Variability-Language/uvl-parser/"
-    "archive/refs/heads/main.zip"
-)
+REPO_ZIP_URL = "https://github.com/Universal-Variability-Language/uvl-parser/" "archive/refs/heads/main.zip"
 
 
 class TestUploader(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox(
-            service=FirefoxService(GeckoDriverManager().install())
-        )
+        self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         self.driver.implicitly_wait(10)
         self.base_url = "http://localhost:5000"
         self.driver.get(self.base_url)
@@ -35,19 +28,14 @@ class TestUploader(unittest.TestCase):
 
         self.driver.set_window_size(1936, 1048)
 
-        self.driver.find_element(
-            By.CSS_SELECTOR, ".nav-link:nth-child(1)"
-            ).click()
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(1)").click()
         self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
         self.driver.find_element(By.ID, "password").send_keys("1234")
         self.driver.find_element(By.ID, "submit").click()
 
         time.sleep(1)
 
-        self.driver.find_element(
-            By.CSS_SELECTOR,
-            ".sidebar-item:nth-child(7) .align-middle:nth-child(2)"
-            ).click()
+        self.driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(7) .align-middle:nth-child(2)").click()
         time.sleep(1)
 
         file_input = self.driver.find_element(By.NAME, "file")
@@ -88,10 +76,8 @@ class TestUploader(unittest.TestCase):
         self.driver.find_element(By.LINK_TEXT, REPO_ZIP_URL).click()
         time.sleep(1)
 
-        self.driver.find_element(
-            By.CSS_SELECTOR, ".list-group-item:nth-child(2) .col-12 > .btn"
-        ).click()
+        self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(2) .col-12 > .btn").click()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
